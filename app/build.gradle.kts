54ea8527c309fd5f1ed2931babe3788bc3eec2dd
yangsb <there-y@qq.com>
2026-04-18 16:49:12 +0800

    修复Lint错误：添加缺失字符串；升级targetSdk至35以符合Google Play要求

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -8,13 +8,13 @@ plugins {
 
 android {
     namespace = "com.rateflip.app"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.rateflip.app"
         minSdk = 26
-        targetSdk = 34
-        versionCode = 1
+        targetSdk = 35
+        versionCode = 2
         versionName = "1.0.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
